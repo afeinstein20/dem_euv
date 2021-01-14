7e2173798385b564079c20cc72e604496061cd9b
Girish M. Duvvuri <girish.duvvuri@gmail.com>
2021-01-14 09:43:11 -0700

    Multiplied continuum emissivities by bin width of wavelength array to match units to G(T) functions. Modified get_gofnt_low_ram to use wavelength bin edges.

--- dem_euv/gofnt_routines.py
+++ dem_euv/gofnt_routines.py
@@ -126,7 +126,7 @@ def initialize_ion(ion):
 
 
 def get_gofnt_matrix_low_ram(ion_strs: List[Any],
-                             wave_arr: np.ndarray, bin_arr: np.ndarray,
+                             wave_lows: np.ndarray, wave_upps: np.ndarray,
                              temp: np.ndarray, dens: np.ndarray,
                              abund_file: str = 'sun_coronal_2012_schmelz_ext',
                              abundance: float = 0.0) -> np.ndarray:
@@ -142,13 +142,13 @@ def get_gofnt_matrix_low_ram(ion_strs: List[Any],
     contribution functions should be evaluated.
     :type ions: list.
 
-    :param wave_arr: Array of the wavelength bin centers for which to evaluate
-    contribution functions.
-    :type wave_arr: np.ndarray.
+    :param wave_lows: Array of the wavelength bin lower bounds
+    for which to evaluate contribution functions.
+    :type wave_lows: np.ndarray.
 
-    :param bin_arr: Array of the wavelength bin widths for which to evaluate
-    and weight the contribution functions.
-    :type wave_arr: np.ndarray.
+    :param wave_upps: Array of the wavelength bin upper bounds
+    for which to evaluate contribution functions.
+    :type wave_upps: np.ndarray.
 
     :param temp: Array of temperatures for which
     the ChiantiPy emissivities have been evaluated.
@@ -163,7 +163,9 @@ def get_gofnt_matrix_low_ram(ion_strs: List[Any],
     divided by wavelength bin width.
 
     """
-    gofnt_matrix = np.zeros((len(wave_arr), len(temp)))
+    gofnt_matrix = np.zeros((len(wave_lows), len(temp)))
+    bin_arr = wave_upps - wave_lows
+    wave_arr = wave_lows + (0.5 * bin_arr)
 
     for ion_str in ion_strs:
         print(ion_str)
@@ -188,13 +190,13 @@ def get_gofnt_matrix_low_ram(ion_strs: List[Any],
             cont.freeFree(wave_arr)
             cont.freeBound(wave_arr)
             if 'intensity' in cont.FreeFree.keys():
-                freefree_contrib = cont.FreeFree['intensity'].T
+                freefree_contrib = cont.FreeFree['intensity'].T * bin_arr
                 ff_mask = np.where(np.isfinite(freefree_contrib))
                 gofnt_matrix[ff_mask] += freefree_contrib[ff_mask]
             else:
                 print('No FreeFree intensity calculated for ',  ion_str)
             if 'intensity' in cont.FreeBound.keys():
-                freebound_contrib = cont.FreeBound['intensity'].T
+                freebound_contrib = cont.FreeBound['intensity'].T * bin_arr
                 fb_mask = np.where(np.isfinite(freebound_contrib))
                 gofnt_matrix[fb_mask] += freebound_contrib[fb_mask]
             else:
